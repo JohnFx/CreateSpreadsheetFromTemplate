9b351965ed292bca1cec9c2668a876a81ab30b4f
John Fuex <john.fuex@gmail.com>
2018-03-13 00:51:23 -0500

    Initial Commit

--- CreateSpreadsheetFromTemplate/Program.cs
+++ CreateSpreadsheetFromTemplate/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CreateSpreadsheetFromTemplate;
+using System.IO;
+using System.Diagnostics;
+
 
 namespace CreateSpreadsheetFromTemplate
 {
@@ -10,6 +14,18 @@ namespace CreateSpreadsheetFromTemplate
     {
         static void Main(string[] args)
         {
+            string rootPath = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, @"..\..\TestData\");
+            string templateFilePath =  Path.Combine(rootPath,"TemplateTest.xlsx");
+            string outputFilePath = Path.Combine(rootPath, "TemplateTest_Result.xlsx");
+            try {
+                if (File.Exists(outputFilePath)) { File.Delete(outputFilePath); }                
+                FileInfo newFile = Excel.CreateFileWithTemplate(new FileInfo(templateFilePath), outputFilePath);
+                Console.WriteLine(string.Format("Created file: {0}",outputFilePath));
+                Process.Start(outputFilePath);
+            }
+        catch (Exception ex) {
+            Console.WriteLine(ex.InnerException.ToString());
         }
+    }
     }
 }
---
+++ CreateSpreadsheetFromTemplate/excel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OfficeOpenXml;
+using System.IO;
+
+namespace CreateSpreadsheetFromTemplate
+{
+    public static class Excel
+    {
+        public static FileInfo CreateFileWithTemplate(FileInfo templateFile, string newFilePath)
+        {
+            try
+            {
+                string ErrorMessage = string.Empty;
+                ExcelPackage xlPackage;
+                FileInfo newFile = new FileInfo(newFilePath);
+
+                using (xlPackage = new ExcelPackage(newFile, templateFile))
+                {
+
+                    ///* Set metadata */
+                    xlPackage.Workbook.Properties.Title = "Sample XLS file from template";
+                    xlPackage.Workbook.Properties.Author = "John Fuex";                                     
+                    xlPackage.Workbook.Properties.Company = "Deloitte Discovery";
+
+                    foreach (ExcelWorksheet ws in xlPackage.Workbook.Worksheets) {
+                        for (int row = 9; row<=20; row++) { 
+                            ws.Cells["A" + row].Value = "John";
+                            ws.Cells["B" + row].Value = "Fuex";
+                            ws.Cells["C" + row].Formula = "=CONCATENATE(A9,\" \", B9)";
+                            ws.Cells["D" + row].Value = 100000 * row;
+                            ws.Cells["D" + row].Style.Numberformat.Format = "$#,##0.00";                                   
+                        }
+                        ws.Cells["D9:D20"].AutoFitColumns();
+                        ws.Cells["D8"].Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
+                }
+                    xlPackage.Workbook.View.ActiveTab = 0;                    
+                    xlPackage.Save();                    
+                }
+                return newFile;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+                return null;
+            }
+        }
+    }
+}
